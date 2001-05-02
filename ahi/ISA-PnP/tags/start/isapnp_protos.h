d29f8d485880868de2387a62bb933a34cd48805b
martin <martin@4f48108a-62f3-0310-b161-d5a2fa85e521>
2001-05-02 16:15:26 +0000

    This commit was manufactured by cvs2svn to create tag 'start'.

---
+++ ahi/ISA-PnP/tags/start/isapnp_protos.h
@@ -0,0 +1,63 @@
+#ifndef CLIB_ISAPNP_PROTOS_H
+#define CLIB_ISAPNP_PROTOS_H
+
+/*
+**      $VER: isapnp_protos.h 1.0 (2.5.2001)
+**
+**      C prototypes. For use with 32 bit integers only.
+**
+**      (C) Copyright 2001 Martin Blom
+**      All Rights Reserved.
+**
+*/
+
+#ifndef EXEC_TYPES_H
+#include <exec/types.h>
+#endif
+
+// Controller functions
+
+void
+ISAC_SetMasterInt( BOOL on );
+
+BOOL
+ISAC_GetMasterInt( void );
+
+void
+ISAC_SetWaitState( BOOL on );
+
+BOOL
+ISAC_GetWaitState( void );
+
+BOOL
+ISAC_GetInterruptStatus( UBYTE interrupt );
+
+UBYTE
+ISAC_GetRegByte( UWORD reg );
+
+void
+ISAC_SetRegByte( UWORD reg, 
+                 UBYTE value );
+
+UWORD
+ISAC_GetRegWord( UWORD reg );
+
+void
+ISAC_SetRegWord( UWORD reg, 
+                 UWORD value );
+
+UBYTE
+ISAC_ReadByte( ULONG address );
+
+void
+ISAC_WriteByte( ULONG address, 
+                UBYTE value );
+
+UWORD
+ISAC_ReadWord( ULONG address );
+
+void
+ISAC_WriteWord( ULONG address, 
+                UWORD value );
+
+#endif /* CLIB_PNPISA_PROTOS_H */
