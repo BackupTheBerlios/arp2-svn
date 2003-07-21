fe3a0b4ba61c1b5a2871c1020d00fd92bd7cde59
martin <martin@4f48108a-62f3-0310-b161-d5a2fa85e521>
2003-07-21 15:21:06 +0000

    Completed varargs support.

--- gg/sfdc/trunk/StubMOS.pl
+++ gg/sfdc/trunk/StubMOS.pl
@@ -37,9 +37,14 @@ BEGIN {
 	    $self->SUPER::function_proto (@_);
 	    print " __attribute__((varargs68k));\n";
 	    print "\n";
+	    $self->SUPER::function_proto (@_);
 	}
-
-	$self->SUPER::function_proto (@_);
+	elsif ($prototype->{type} eq 'stdarg') {
+	    # Do nothing
+	}
+	else {
+	    $self->SUPER::function_proto (@_);
+	}
     }
     
     sub function_start {
@@ -65,6 +70,71 @@ BEGIN {
 	    print "  va_start (_va, $prototype->{___argnames}[$na-2]);\n";
 	    print "  return $$prototype{'real_funcname'}(BASE_PAR_NAME ";
 	}
+	elsif ($prototype->{type} eq 'stdarg') {
+	    # Shamelessly stolen from fd2inline ...
+
+	    # number of regs that contain varargs
+	    my $n = 9 - $prototype->{numregs};
+
+	    # add 4 bytes if that's an odd number, to avoid splitting a tag
+	    my $d = $n & 1 ? 4 : 0;
+
+	    # offset of the start of the taglist
+	    my $taglist = 8;
+
+	    # size of the stack frame
+	    my $local = ($taglist + $n * 4 + $d + 8 + 15) & ~15;
+
+	    #  Stack frame:
+	    #
+	    #   0 -  3: next frame ptr
+	    #   4 -  7: save lr
+	    #   8 -  8+n*4+d+8-1: tag list start
+	    #   ? - local-1: padding
+
+	    print "__asm(\"\n";
+	    print "	.align	2\n";
+	    print "	.globl	$prototype->{funcname}\n";
+	    print "	.type	$prototype->{funcname},\@function\n";
+	    print "$prototype->{funcname}:\n";
+	    print "	stwu	1,-$local(1)\n";
+	    print "	mflr	0\n";
+	    printf "	stw	0,%d(1)\n", $local + 4;
+
+	    # If n is odd, one tag is split between regs and stack.
+	    # Copy its ti_Data together with the ti_Tag.
+	    
+	    if ($d != 0) {
+		# read ti_Data
+		printf "	lwz	0,%d(1)\n", $local + 8;
+	    }
+
+	    # Save the registers
+	    
+	    for my $count ($prototype->{numregs} .. 8) {
+		printf "	stw	%d,%d(1)\n",
+		$count + 2,
+		($count - $prototype->{numregs}) * 4 + $taglist;
+	    }
+
+	    if ($d != 0) {
+		# write ti_Data
+		printf "	stw	0,%d(1)\n", $taglist + $n * 4;
+	    }
+
+	    # Add TAG_MORE
+
+	    print "	li	11,2\n";
+	    printf "	addi	0,1,%d\n", $local + 8 + $d;
+	    printf "	stw	11,%d(1)\n", $taglist + $n * 4 + $d;
+	    printf "	stw	0,%d(1)\n", $taglist + $n * 4 + $d + 4;
+
+	    # vararg_reg = &saved regs
+	    
+	    printf "	addi	%d,1,%d\n",
+	    $prototype->{numregs} + 2, $taglist;
+	    print "	bl	$prototype->{real_funcname}\n";
+	}
 	else {
 	    $self->SUPER::function_start (@_);
 	}
@@ -88,6 +158,28 @@ BEGIN {
 	    my $vartype  = $$prototype{'argtypes'}[$$prototype{'numargs'} - 1];
 	    my $argnm = $$prototype{'___argnames'}[$$prototype{'numargs'} - 2];
 	    print ", ($vartype) _va->overflow_arg_area";
+	}
+	elsif ($prototype->{type} eq 'stdarg') {
+#	    if ($argnum < $prototype->{numargs} - 2) {
+#		my $regoffset;
+#
+#		if ($argreg =~ /^d[0-9]$/) {
+#		    ( $regoffset = $argreg ) =~ s/^d//;
+#		}
+#		elsif ($argreg =~ /^a[0-9]$/) {
+#		    ( $regoffset = $argreg ) =~ s/^a//;
+#		    $regoffset += 8;
+#		}
+#		else {
+#		    die;
+#		}
+#
+#		$regoffset *= 4;
+#
+#		# Save the non-varargs registers in the EmulHandle struct
+#
+#		printf "	stw	%d,%d(2)\n", $argnum + 3, $regoffset;
+#	    }
 	}
 	else {
 	    $self->SUPER::function_arg (@_);
@@ -117,6 +209,30 @@ BEGIN {
 
 	    print "(*MyEmulHandle->EmulCallDirectOS)(-$prototype->{bias});\n";
 	    print "}\n";
+	}
+	elsif ($prototype->{type} eq 'stdarg') {
+	    # number of regs that contain varargs
+	    my $n = 9 - $prototype->{numregs};
+
+	    # add 4 bytes if that's an odd number, to avoid splitting a tag
+	    my $d = $n & 1 ? 4 : 0;
+
+	    # offset of the start of the taglist
+	    my $taglist = 8;
+
+	    # size of the stack frame
+	    my $local = ($taglist + $n * 4 + $d + 8 + 15) & ~15;
+
+	    # clear stack frame & return
+	    printf "	lwz	0,%d(1)\n", $local + 4;
+	    print "	mtlr	0\n";
+	    printf "	addi	1,1,%d\n", $local;
+	    print "	blr\n";
+	    print ".L$prototype->{funcname}e1:\n";
+	    print "	.size	$prototype->{funcname}," .
+		".L$prototype->{funcname}e1-$prototype->{funcname}\n";
+	    
+	    print "\");\n"
 	}
 	else {
 	    $self->SUPER::function_end (@_);
