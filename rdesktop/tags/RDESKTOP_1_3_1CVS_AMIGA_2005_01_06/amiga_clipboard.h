31dfcc62dc2a6b96cdd50e72cca54e82d7c4ff1c
martin <martin@4f48108a-62f3-0310-b161-d5a2fa85e521>
2005-01-06 21:52:39 +0000

    This commit was manufactured by cvs2svn to create tag 'RDESKTOP_1_3_1CVS_AMIGA_2005_01_06'.

---
+++ rdesktop/tags/RDESKTOP_1_3_1CVS_AMIGA_2005_01_06/amiga_clipboard.h
@@ -0,0 +1,12 @@
+#ifndef RDESKTOP_AMIGA_CLIPBOARD_H
+#define RDESKTOP_AMIGA_CLIPBOARD_H
+
+#include <exec/ports.h>
+
+BOOL amiga_clip_init(void);
+void amiga_clip_deinit(void);
+void amiga_clip_handle_signals();
+
+extern ULONG amiga_clip_signals;
+
+#endif /* RDESKTOP_AMIGA_CLIPBOARD_H */
