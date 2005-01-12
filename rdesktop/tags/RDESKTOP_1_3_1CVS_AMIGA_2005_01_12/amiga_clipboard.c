4cccb85dfbb073aa6a849b54d76c6d66464b0fff
martin <martin@4f48108a-62f3-0310-b161-d5a2fa85e521>
2005-01-12 14:06:59 +0000

    This commit was manufactured by cvs2svn to create tag 'RDESKTOP_1_3_1CVS_AMIGA_2005_01_12'.

---
+++ rdesktop/tags/RDESKTOP_1_3_1CVS_AMIGA_2005_01_12/amiga_clipboard.c
@@ -0,0 +1,360 @@
+/*
+  rdesktop: A Remote Desktop Protocol client.
+  Clipboard handling - Amiga
+  Copyright (C) Martin Blom 2004-2005
+   
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+   
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "rdesktop.h"
+
+#undef BOOL
+#define Bool int
+
+#include <devices/clipboard.h>
+#include <libraries/iffparse.h>
+#include <utility/hooks.h>
+
+#include <proto/exec.h>
+#include <proto/iffparse.h>
+#include <clib/alib_protos.h>
+
+#include <stdio.h>
+#include "amiga_clipboard.h"
+
+#define ID_FTXT MAKE_ID('F', 'T', 'X', 'T')
+#define ID_CHRS MAKE_ID('C', 'H', 'R', 'S')
+
+ULONG amiga_clip_signals = 0;
+
+static BYTE                    amiga_clip_hooksignal = -1;
+static struct ClipboardHandle* amiga_clip_handle     = NULL;
+static struct IFFHandle*       amiga_clip_iffhandle  = NULL;
+static LONG                    amiga_clip_announce   = 0;
+static LONG                    amiga_clip_clipid     = 0;
+static ULONG                   amiga_clip_format     = CF_TEXT;
+
+static ULONG amiga_clip_hookfunc(struct Hook*        hook,
+				 APTR                object,
+				 struct ClipHookMsg* msg) {
+  if (msg->chm_Type == 0) {
+    // Ask main processing loop to call amiga_clip_handle_signals()
+    amiga_clip_announce = msg->chm_ClipID;
+    Signal((struct Task*) hook->h_Data, 1UL << amiga_clip_hooksignal);
+  }
+
+  return 0;
+}
+
+static struct Hook amiga_clip_hook = {
+  { NULL, NULL },
+#ifdef __amigaos4__
+  amiga_clip_hookfunc,
+  NULL,
+#else
+  HookEntry,
+  amiga_clip_hookfunc,
+#endif
+  NULL
+};
+
+
+static BOOL amiga_clip_active_post() {
+  if (amiga_clip_clipid != 0) {
+    // We may have an outstanding CBD_POST. Check it.
+
+    amiga_clip_handle->cbh_Req.io_Command = CBD_CURRENTWRITEID;
+    DoIO((struct IORequest*) &amiga_clip_handle->cbh_Req);
+ 
+    if (amiga_clip_handle->cbh_Req.io_ClipID <= amiga_clip_clipid) {
+      return TRUE;
+    }
+  }
+
+  return FALSE;
+}
+
+BOOL
+amiga_clip_init(void) {
+  amiga_clip_hooksignal = AllocSignal(-1);
+  
+  if (amiga_clip_hooksignal == -1) {
+    return FALSE;
+  }
+
+  amiga_clip_iffhandle = AllocIFF();
+
+  if (amiga_clip_iffhandle == NULL) {
+    return FALSE;
+  }
+  
+  amiga_clip_handle = OpenClipboard(PRIMARY_CLIP);
+
+  if (amiga_clip_handle == NULL) {
+    return FALSE;
+  }
+
+  amiga_clip_iffhandle->iff_Stream = (ULONG) amiga_clip_handle;
+  InitIFFasClip(amiga_clip_iffhandle);
+      
+  amiga_clip_signals = (//(1UL << amiga_clip_handle->cbh_CBport.mp_SigBit) |
+			(1UL << amiga_clip_handle->cbh_SatisfyPort.mp_SigBit) |
+			(1UL << amiga_clip_hooksignal));
+      
+  // Add clipboard hook
+  amiga_clip_hook.h_Data = FindTask(NULL);
+  
+  amiga_clip_handle->cbh_Req.io_Command = CBD_CHANGEHOOK;
+  amiga_clip_handle->cbh_Req.io_Data    = (char*) &amiga_clip_hook;
+  amiga_clip_handle->cbh_Req.io_Length  = 1;
+  DoIO((struct IORequest*) &amiga_clip_handle->cbh_Req);
+  
+  return TRUE;
+}
+
+BOOL
+amiga_clip_shutdown(void) {
+  if (amiga_clip_active_post()) {
+    static BOOL once = TRUE;
+
+    if (once) {
+      cliprdr_send_data_request(amiga_clip_format);
+      once = FALSE;
+    }
+
+    // Don't shutdown yet
+    return FALSE;
+  }
+  else {
+    // It's ok to shutdown
+    return TRUE;
+  }
+}
+
+void
+amiga_clip_deinit(void) {
+  if (amiga_clip_active_post()) {
+    // We have an outstanding CBD_POST! This happens when we
+    // disconnect using Windows and the Amiga side.  All we can do
+    // is to write empty data to the clipboard. :-(
+    ui_clip_handle_data("", 0);
+  }
+  
+  if (amiga_clip_iffhandle != NULL) {
+    FreeIFF(amiga_clip_iffhandle);
+  }
+
+  if (amiga_clip_handle != NULL) {
+    // Remove clipboard hook
+    amiga_clip_hook.h_Data = NULL;
+
+    amiga_clip_handle->cbh_Req.io_Command = CBD_CHANGEHOOK;
+    amiga_clip_handle->cbh_Req.io_Data    = (char*) &amiga_clip_hook;
+    amiga_clip_handle->cbh_Req.io_Length  = 0;
+    DoIO((struct IORequest*) &amiga_clip_handle->cbh_Req);
+
+    CloseClipboard(amiga_clip_handle);
+    amiga_clip_handle  = NULL;
+    amiga_clip_signals = 0;
+  }
+
+  FreeSignal(amiga_clip_hooksignal);
+}
+
+
+void
+amiga_clip_handle_signals() {
+  struct SatisfyMsg* sm;
+
+  if (amiga_clip_announce != 0 && amiga_clip_announce != amiga_clip_clipid) {
+    amiga_clip_announce = 0;
+    ui_clip_sync();
+  }
+
+  sm = (struct SatisfyMsg*) GetMsg(&amiga_clip_handle->cbh_SatisfyPort);
+  
+  if (sm != NULL) {
+    // TODO: In rare cases, the Windows host will never reply to this
+    // request. In that case, the Amiga clipboard will hang until some
+    // other application pastes.
+    cliprdr_send_data_request(amiga_clip_format);
+  }
+}
+
+
+void
+ui_clip_format_announce(uint8 * data, uint32 length)
+{
+  if ((length % 36) != 0) {
+    // What's this???
+    return;
+  }
+
+  while (length != 0) {
+    uint32 format = ((data[3] << 24) |
+		     (data[2] << 16) |
+		     (data[1] <<  8) |
+		     (data[0]));
+    
+    if (format == CF_TEXT) {
+      amiga_clip_handle->cbh_Req.io_Command = CBD_POST;
+      amiga_clip_handle->cbh_Req.io_Data    =
+	(char*) &amiga_clip_handle->cbh_SatisfyPort;
+      amiga_clip_handle->cbh_Req.io_ClipID  = 0;
+
+      if (DoIO((struct IORequest*) &amiga_clip_handle->cbh_Req) == 0) {
+	amiga_clip_format = format;
+	amiga_clip_clipid = amiga_clip_handle->cbh_Req.io_ClipID;
+      }
+      else {
+	amiga_clip_clipid = 0;
+      }
+
+      break;
+    }
+    
+    length -= 36;
+    data += 36;
+  }
+  
+}
+
+void ui_clip_handle_data(uint8 * data, uint32 length)
+{
+  if (amiga_clip_format == CF_TEXT) {
+    if (OpenIFF(amiga_clip_iffhandle, IFFF_WRITE) == 0) {
+
+      amiga_clip_handle->cbh_Req.io_ClipID = amiga_clip_clipid;
+      amiga_clip_clipid = 0;
+      
+      if (PushChunk(amiga_clip_iffhandle, ID_FTXT, ID_FORM,
+		    IFFSIZE_UNKNOWN) == 0) {
+	if (PushChunk(amiga_clip_iffhandle, 0, ID_CHRS,
+		      IFFSIZE_UNKNOWN) == 0) {
+	  int len;
+	  int i;
+
+	  for (i = 0, len = 0; i < length && data[i] != 0; ++i) {
+	    if (data[i] != '\r') {
+	      data[len] = data[i];
+	      ++len;
+	    }
+	  }
+
+	  if (WriteChunkBytes(amiga_clip_iffhandle, data, len) == len) {
+	    // Success
+	  }
+
+	  PopChunk(amiga_clip_iffhandle);
+	}
+
+	PopChunk(amiga_clip_iffhandle);
+      }
+      
+      CloseIFF(amiga_clip_iffhandle);
+    }
+  }
+}
+
+static char* extract_collection(struct CollectionItem* ci, char* str) {
+  int   i;
+  char* chrs = ci->ci_Data;
+
+  if (ci->ci_Next != NULL) {
+    str = extract_collection(ci->ci_Next, str);
+  }
+  
+  for (i = 0; i < ci->ci_Size; ++i) {
+    if (chrs[i] == '\n') {
+      *str++ = '\r';
+    }
+
+    *str++ = chrs[i];
+  }
+
+  *str = 0;
+  return str;
+}
+
+void ui_clip_request_data(uint32 format)
+{
+  char* str = NULL;
+  int	len = 1; // Always reserve space for NUL byte
+
+  if (format == CF_TEXT) {
+    if (!amiga_clip_active_post() && // Avoid deadlocks
+	OpenIFF(amiga_clip_iffhandle, IFFF_READ) == 0) {
+      if (CollectionChunk(amiga_clip_iffhandle, ID_FTXT, ID_CHRS) == 0 &&
+	  StopOnExit(amiga_clip_iffhandle,ID_FTXT,ID_FORM) == 0) {
+	while (TRUE) {
+	  LONG error = ParseIFF(amiga_clip_iffhandle, IFFPARSE_SCAN);
+
+	  if (error == IFFERR_EOF) {
+	    break;
+	  }
+	  else if (error == IFFERR_EOC) {
+	    struct CollectionItem* ci;
+	    struct CollectionItem* first_ci;
+
+ 	    ci = first_ci = FindCollection(amiga_clip_iffhandle,
+					   ID_FTXT, ID_CHRS);
+
+	    while (ci != NULL) {
+	      int   i;
+	      char* chrs = ci->ci_Data;
+
+	      len += ci->ci_Size;
+
+	      for (i = 0; i < ci->ci_Size; ++i) {
+		if (chrs[i] == '\n') {
+		  ++len;
+		}
+	      }
+
+	      ci = ci->ci_Next;
+	    }
+
+	    str = malloc(len);
+
+	    if (str != NULL) {
+	      extract_collection(first_ci, str);
+	    }
+ 	  }
+	  else {
+	    // Something is wrong!
+	    break;
+	  }
+	}
+      }
+      
+      CloseIFF(amiga_clip_iffhandle);
+    }
+
+    if (str != NULL) {
+      cliprdr_send_data(str, len);
+      free(str);
+    }
+    else {
+      cliprdr_send_data("", 0);
+    }
+
+  }
+}
+
+void ui_clip_sync(void)
+{
+  // TODO: Check format of all clipboard.device data here
+  cliprdr_send_text_format_announce();
+}
---
+++ rdesktop/tags/RDESKTOP_1_3_1CVS_AMIGA_2005_01_12/rdesktop-2004-04-17-0600/types.h
@@ -0,0 +1,221 @@
+/*
+   rdesktop: A Remote Desktop Protocol client.
+   Common data types
+   Copyright (C) Matthew Chapman 1999-2002
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef True
+#define True  (1)
+#define False (0)
+#endif
+
+#ifdef __amigaos4__
+   #include <exec/types.h>
+#else
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+#endif
+typedef signed char sint8;
+typedef signed short sint16;
+typedef signed int sint32;
+
+#define BOOL int
+
+typedef void *HBITMAP;
+typedef void *HGLYPH;
+typedef void *HCOLOURMAP;
+typedef void *HCURSOR;
+
+typedef struct _COLOURENTRY
+{
+	uint8 red;
+	uint8 green;
+	uint8 blue;
+
+}
+COLOURENTRY;
+
+typedef struct _COLOURMAP
+{
+	uint16 ncolours;
+	COLOURENTRY *colours;
+
+}
+COLOURMAP;
+
+typedef struct _BOUNDS
+{
+	sint16 left;
+	sint16 top;
+	sint16 right;
+	sint16 bottom;
+
+}
+BOUNDS;
+
+typedef struct _PEN
+{
+	uint8 style;
+	uint8 width;
+	uint32 colour;
+
+}
+PEN;
+
+typedef struct _BRUSH
+{
+	uint8 xorigin;
+	uint8 yorigin;
+	uint8 style;
+	uint8 pattern[8];
+
+}
+BRUSH;
+
+typedef struct _FONTGLYPH
+{
+	sint16 offset;
+	sint16 baseline;
+	uint16 width;
+	uint16 height;
+	HBITMAP pixmap;
+
+}
+FONTGLYPH;
+
+typedef struct _DATABLOB
+{
+	void *data;
+	int size;
+
+}
+DATABLOB;
+
+typedef struct _key_translation
+{
+	uint8 scancode;
+	uint16 modifiers;
+}
+key_translation;
+
+typedef struct _VCHANNEL
+{
+	uint16 mcs_id;
+	char name[8];
+	uint32 flags;
+	struct stream in;
+	void (*process) (STREAM);
+}
+VCHANNEL;
+
+#define MAX_CBSIZE 256
+
+/* RDPSND */
+typedef struct
+{
+	uint16 wFormatTag;
+	uint16 nChannels;
+	uint32 nSamplesPerSec;
+	uint32 nAvgBytesPerSec;
+	uint16 nBlockAlign;
+	uint16 wBitsPerSample;
+	uint16 cbSize;
+	uint8 cb[MAX_CBSIZE];
+} WAVEFORMATEX;
+
+/* RDPDR */
+typedef uint32 NTSTATUS;
+typedef uint32 HANDLE;
+
+typedef struct _DEVICE_FNS
+{
+	NTSTATUS(*create) (uint32 device, uint32 desired_access, uint32 share_mode,
+			   uint32 create_disposition, uint32 flags_and_attributes, char *filename,
+			   HANDLE * handle);
+	NTSTATUS(*close) (HANDLE handle);
+	NTSTATUS(*read) (HANDLE handle, uint8 * data, uint32 length, uint32 offset,
+			 uint32 * result);
+	NTSTATUS(*write) (HANDLE handle, uint8 * data, uint32 length, uint32 offset,
+			  uint32 * result);
+	NTSTATUS(*device_control) (HANDLE handle, uint32 request, STREAM in, STREAM out);
+}
+DEVICE_FNS;
+
+
+typedef struct rdpdr_device_info
+{
+	uint32 device_type;
+	HANDLE handle;
+	char name[8];
+	char *local_path;
+	void *pdevice_data;
+}
+RDPDR_DEVICE;
+
+typedef struct rdpdr_serial_device_info
+{
+	int dtr;
+	int rts;
+	uint32 baud_rate,
+		queue_in_size,
+		queue_out_size,
+		wait_mask,
+		read_interval_timeout,
+		read_total_timeout_multiplier,
+		read_total_timeout_constant,
+		write_total_timeout_multiplier, write_total_timeout_constant, posix_wait_mask;
+	uint8 stop_bits, parity, word_length;
+	struct termios *ptermios, *pold_termios;
+}
+SERIAL_DEVICE;
+
+typedef struct rdpdr_parallel_device_info
+{
+	char *driver, *printer;
+	uint32 queue_in_size,
+		queue_out_size,
+		wait_mask,
+		read_interval_timeout,
+		read_total_timeout_multiplier,
+		read_total_timeout_constant,
+		write_total_timeout_multiplier,
+		write_total_timeout_constant, posix_wait_mask, bloblen;
+	uint8 *blob;
+}
+PARALLEL_DEVICE;
+
+typedef struct rdpdr_printer_info
+{
+	FILE *printer_fp;
+	char *driver, *printer;
+	uint32 bloblen;
+	uint8 *blob;
+	BOOL default_printer;
+}
+PRINTER;
+
+typedef struct fileinfo
+{
+	uint32 device_id, flags_and_attributes;
+	char path[256];
+	DIR *pdir;
+	struct dirent *pdirent;
+	char pattern[64];
+	BOOL delete_on_close;
+}
+FILEINFO;
