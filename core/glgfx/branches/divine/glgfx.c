b06f71396b7a30af02990940efb1d2a29834f328
martin <martin@4f48108a-62f3-0310-b161-d5a2fa85e521>
2005-03-03 22:47:03 +0000

    This commit was manufactured by cvs2svn to create branch 'divine'.

---
+++ core/glgfx/branches/divine/glgfx.c
@@ -0,0 +1,110 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include "glgfx.h"
+#include "glgfx_monitor.h"
+
+#define max_monitors  8               // Four cards, two outputs/card max
+static int            num_monitors;
+struct glgfx_monitor* monitors[max_monitors];
+
+bool glgfx_create_monitors(void) {
+  char name[8];
+  int display;
+  int screen;
+  
+  glgfx_destroy_monitors();
+
+  num_monitors = 0;
+  
+  for (display = 0; num_monitors < max_monitors; ++display) {
+    for (screen = 0; num_monitors < max_monitors; ++screen) {
+      sprintf(name, ":%d.%d", display, screen);
+
+      monitors[num_monitors] = glgfx_monitor_create(name);
+
+      if (monitors[num_monitors] == NULL) {
+	if (errno == ENXIO) {
+	  break;
+	}
+	else {
+	  continue;
+	}
+      }
+
+      int i;
+      for (i = 0; i < 160; ++i) {
+	glgfx_monitor_select(monitors[num_monitors]);
+	glDrawBuffer(GL_FRONT);
+	glClearColor( i/160.0, 0, 0, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glFlush();
+	glgfx_monitor_waittof(monitors[num_monitors]);
+      }
+      
+      ++num_monitors;
+    }
+
+    if (screen == 0) {
+      // Failed to open display ":x.0"
+      break;
+    }
+  }
+
+  return num_monitors != 0;
+}
+
+
+void glgfx_destroy_monitors(void) {
+  int i;
+
+  for (i = 0; i < num_monitors; ++i) {
+    glgfx_monitor_destroy(monitors[i]);
+    monitors[i] = NULL;
+  }
+
+  num_monitors = 0;
+}
+
+
+bool glgfx_waitblit(void) {
+  bool rc = true;
+  int i;
+
+  for (i = 0; i < num_monitors; ++i) {
+    if (!glgfx_monitor_waitblit(monitors[i])) {
+      rc = false;
+    }
+  }
+
+  return rc;
+}
+
+bool glgfx_waittof(int monitor) {
+  bool rc = true;
+  int i;
+
+  for (i = 0; i < num_monitors; ++i) {
+    if (!glgfx_monitor_waittof(monitors[i])) {
+      rc = false;
+    }
+  }
+
+  return rc;
+}
+
+
+int main(int argc, char** argv) {
+  // If unset, sync to vblank as default (nvidia driver)
+  setenv("__GL_SYNC_TO_VBLANK", "1", False);
+/*   if (glopen()) { */
+    if (glgfx_create_monitors()) {
+
+      glgfx_destroy_monitors();
+    }
+/*   } */
+  
+  return 0;
+}
---
+++ core/glgfx/branches/divine/glgfx_monitor.h
@@ -0,0 +1,32 @@
+#ifndef arp2_glgfx_glgfx_monitor_h
+#define arp2_glgfx_glgfx_monitor_h
+
+#include "glgfx.h"
+
+#include <X11/Xlib.h>
+#include <X11/Xatom.h>
+#include <X11/extensions/xf86vmode.h>
+#include <GL/glx.h>
+
+struct glgfx_monitor {
+    char const*          name;
+    Display*             display;
+    Window               window;
+    GLXContext           context;
+
+    Atom                 xa_win_state;
+
+    XF86VidModeModeLine  mode;
+    int                  dotclock;
+    XF86VidModeMonitor   monitor_info;
+};
+
+struct glgfx_monitor* glgfx_monitor_create(char const* display_name);
+void glgfx_monitor_destroy(struct glgfx_monitor* monitor);
+void glgfx_monitor_fullscreen(struct glgfx_monitor* monitor, bool fullscreen);
+bool glgfx_monitor_select(struct glgfx_monitor* monitor);
+bool glgfx_monitor_waitblit(struct glgfx_monitor* monitor);
+bool glgfx_monitor_waittof(struct glgfx_monitor* monitor);
+
+#endif /* arp2_glgfx_glgfx_monitor_h */
+
