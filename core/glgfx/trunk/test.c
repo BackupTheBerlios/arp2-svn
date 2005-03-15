191d94c098ebdd6ece10d1abbb6e2611cdcc1852
martin <martin@4f48108a-62f3-0310-b161-d5a2fa85e521>
2005-03-15 22:47:40 +0000

    Now prints performance stats. 16 bit/pixel test.

--- core/glgfx/trunk/test.c
+++ core/glgfx/trunk/test.c
@@ -8,10 +8,12 @@
 #include "glgfx_viewport.h"
 #include "glgfx_input.h"
 
+#define UPLOAD_MODE 2
+
 int main(int argc __attribute__((unused)), char** argv __attribute__((unused))) {
   // If unset, sync to vblank as default (nvidia driver)
-  setenv("__GL_SYNC_TO_VBLANK", "1", False);
-  setenv("__GL_NV30_EMULATE", "1", False);
+  setenv("__GL_SYNC_TO_VBLANK", "0", False);
+  setenv("__GL_NV30_EMULATE", "0", False);
 
 /* Section "Device" */
 /*     Identifier "NV AGP" */
@@ -22,23 +24,39 @@ int main(int argc __attribute__((unused)), char** argv __attribute__((unused))) 
 /* EndSection */
 
   if (glgfx_create_monitors()) {
-    int width = 640;
-    int height = 480;
+    int width = 1024;
+    int height = 768;
+    unsigned short* data;
+
+#if UPLOAD_MODE < 2
+    data = calloc(sizeof (*data), width * height);
+#endif
+    
     struct glgfx_bitmap* bm = glgfx_bitmap_create(width, height, 24, 0, NULL,
-						  glgfx_pixel_b8g8r8a8, glgfx_monitors[0]);
+						  glgfx_pixel_a4r4g4b4, glgfx_monitors[0]);
 
     if (bm != NULL) {
-	if (glgfx_bitmap_lock(bm, false, true)) {
-	  int* data = (int*) bm->locked_memory;
-	  int x, y;
-
-	  for (y = 0; y < height; y+=1) {
-	    for (x = 0; x < width; x+=1) {
-	      data[x+y*width] = (255 << 24) | ((255*x/width) << 16) | ((255*y/height) << 8) | 0;
-	    }
+#if UPLOAD_MODE > 0
+      if (glgfx_bitmap_lock(bm, false, true)) {
+#endif
+	int x, y;
+      
+#if UPLOAD_MODE > 1
+	data = glgfx_bitmap_map(bm);
+#endif
+	for (y = 0; y < height; y+=1) {
+	  for (x = 0; x < width; x+=1) {
+//	    data[x+y*width] = (255 << 24) | ((255*x/width) << 16) | ((255*y/height) << 8) | 0;
+	    data[x+y*width] = 0x8ff8;
 	  }
-	  glgfx_bitmap_unlock(bm);
 	}
+#if UPLOAD_MODE < 2
+	  glgfx_bitmap_update(bm, data, width*height*sizeof (*data));
+#endif
+#if UPLOAD_MODE > 0
+	glgfx_bitmap_unlock(bm);
+      }
+#endif
 
       glgfx_monitor_select(glgfx_monitors[0]);
 
@@ -48,20 +66,32 @@ int main(int argc __attribute__((unused)), char** argv __attribute__((unused))) 
 
       glgfx_view_addviewport(v, vp);
 
-      glgfx_input_acquire();
+//      glgfx_input_acquire();
       int i;
-      for (i = 0; i < 255; i+=1) {
-/* 	if (glgfx_bitmap_lock(bm, false, true)) { */
-/* 	  int* data = (int*) bm->locked_memory; */
-/* 	  int x, y; */
-      
-/* 	  for (y = 0; y < height; y+=10) { */
-/* 	    for (x = 0; x < width; x+=10) { */
-/* 	      data[x+y*width+i*13] = -1; */
-/* 	    } */
-/* 	  } */
-/* 	  glgfx_bitmap_unlock(bm); */
-/* 	} */
+      struct timeval s, e;
+
+      gettimeofday(&s, NULL);
+      for (i = 0; i < 256; i+=1) {
+#if UPLOAD_MODE > 0
+	if (glgfx_bitmap_lock(bm, false, true)) {
+#endif
+	  int x, y;
+
+#if UPLOAD_MODE > 1
+	  data = glgfx_bitmap_map(bm);
+#endif
+	  for (y = 0; y < height; y+=10) {
+	    for (x = 0; x < width; x+=10) {
+	      data[x+y*width] = -1;
+	    }
+	  }
+#if UPLOAD_MODE < 2
+	  glgfx_bitmap_update(bm, data, width*height*sizeof (*data));
+#endif
+#if UPLOAD_MODE > 0
+	  glgfx_bitmap_unlock(bm);
+	}
+#endif
 
 	glgfx_viewport_move(vp, 320 + i, 256, 100, 200+i);
 //	glgfx_viewport_setbitmap(vp, ri, bm, 0, 0, 320, 256);
@@ -69,12 +99,17 @@ int main(int argc __attribute__((unused)), char** argv __attribute__((unused))) 
 	glgfx_view_render(v);
 	glgfx_monitor_waittof(glgfx_monitors[0]);
 
-	enum glgfx_input_code code;
-	while ((code = glgfx_input_getcode()) != glgfx_input_none) {
-	  printf("%08lx\n", code);
-	}
+/* 	enum glgfx_input_code code; */
+/* 	while ((code = glgfx_input_getcode()) != glgfx_input_none) { */
+/* 	  printf("%08lx\n", code); */
+/* 	} */
       }
-      glgfx_input_release();
+      gettimeofday(&e, NULL);
+      double sec = (e.tv_sec + e.tv_usec * 1e-6) - (s.tv_sec + s.tv_usec * 1e-6);
+      
+      printf("uploaded %d images in %g seconds -> %g fps/%d MB/s\n",
+	     i, sec, i / sec, (int) (i * width * height * sizeof (*data) / sec / 1e6));
+//      glgfx_input_release();
 
       glgfx_viewport_destroy(vp);
     }
