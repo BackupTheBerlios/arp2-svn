4f629aac49fa240b204acab67fa9e4c5f0d6121b
martin <martin@4f48108a-62f3-0310-b161-d5a2fa85e521>
2005-03-29 18:21:00 +0000

    Tag gg/flexcat/trunk as gg/flexcat/tags/flexcat-2.6.6.20050112-1.

---
+++ gg/flexcat/tags/flexcat-2.6.6.20050112-1/utils.h
@@ -0,0 +1,47 @@
+
+/* $Id: utils.h,v 1.5 2003/02/19 23:03:28 amiandrew Exp $
+ * 
+ * Copyright (C) 2002 Ondrej Zima <amiandrew@volny.cz>
+ * Copyright (C) 2002 Stefan Kost <ensonic@sonicpulse.de>
+ * Copyright (C) 1993 Jochen Wiedmann and Marcin Orlowski <carlos@wfmh.org.pl>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#ifndef FLEXCAT_UTILS_H
+#define FLEXCAT_UTILS_H
+
+// Functions
+void            MyExit ( int Code );
+char           *AllocString ( const char *str );
+char           *AddCatalogChunk ( char *ID, const char *string );
+int             gethex ( int c );
+int             getoctal ( int c );
+char           *ReadLine ( FILE * fp, int AllowComment );
+void            OverSpace ( char **strptr );
+void            Expunge ( void );
+int             ReadChar ( char **strptr, char *dest );
+char           *AllocFileName ( char *filename, int howto );
+char           *AddFileName ( char *pathname, char *filename );
+void            Usage ( void );
+
+#ifndef __amigados
+int             Stricmp ( const char *str1, const char *str2 );
+int             Strnicmp ( const char *str1, const char *str2,
+                           register int len );
+#endif
+
+#endif
