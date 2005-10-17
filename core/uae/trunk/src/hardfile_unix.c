58f25201eaf31b4bfc1aef686c79bc52a0a99e61
leviticus <leviticus@4f48108a-62f3-0310-b161-d5a2fa85e521>
2005-10-17 19:25:48 +0000

    r854@matilda:  martin | 2005-10-15 12:44:59 +0200  Use AC_SYS_LARGEFILE for 64-bit hardfile support.

--- core/uae/trunk/src/hardfile_unix.c
+++ core/uae/trunk/src/hardfile_unix.c
@@ -10,8 +10,6 @@
   * support still to come . . .
   */
 
-#define _FILE_OFFSET_BITS 64
-
 #include "sysconfig.h"
 #include "sysdeps.h"
 
@@ -47,7 +45,7 @@ static int hdf_seek (struct hardfiledata *hfd, uae_u64 offset)
 	abort ();
     }
 
-    if (offset >= 0x80000000 && _FILE_OFFSET_BITS != 64) {
+    if (offset >= 0x80000000 && sizeof (off_t) < sizeof (uae_u64)) {
 	DEBUG_LOG ("Failed to seek passed 2GB limit (0x%llx)\n", offset);
 	return -1;
     }
