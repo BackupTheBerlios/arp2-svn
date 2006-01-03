1507b471086ad4afe3a3d4c1b84e7c50bcdd53a8
leviticus <leviticus@4f48108a-62f3-0310-b161-d5a2fa85e521>
2006-01-03 20:32:34 +0000

    r2264@lisa:  martin | 2006-01-03 21:32:26 +0100  WIP

--- core/arp2-irq/pcode-test.c
+++ core/arp2-irq/pcode-test.c
@@ -23,10 +23,12 @@ int init(struct Locals* l) {
 
 int acknowledge(struct Locals* l) {
   uint16_t intreq = ReadResource16(13, l->intreqr);
+  intreq = bswap16(intreq);
 
   if (intreq & 0x0080) {
     intreq &= ~0x0080;
 
+    SetEndian(ENDIAN_BIG);
     WriteResource16(12, l->intreq, intreq);
     return 1;
   }
@@ -36,5 +38,19 @@ int acknowledge(struct Locals* l) {
 }
 
 int release() { 
+  uint64_t  dummy;
+  uint64_t* fb = MapResource(0, MAPF_WRITE, &dummy);
+  uint64_t  sz = GetResourceSize(0) / sizeof (uint64_t);
+
+  // Clear frame buffer
+  if (fb != NULL) {
+    size_t i;
+    for (i = 0; i < sz; ++i) {
+      fb[i] = 0;
+    }
+  }
+
+  UnmapResource(fb);
+  
   return 0; 
 }
--- core/arp2-irq/pcode-traps.h
+++ core/arp2-irq/pcode-traps.h
@@ -1,15 +1,35 @@
 #ifndef pcode_calls_h
 #define pcode_calls_h
 
+/*** Type definitions ********************************************************/
+
 typedef unsigned char  uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int   uint32_t;
 typedef unsigned long  uint64_t;
+typedef unsigned long  size_t;
+typedef long           ssize_t;
 
-#define _readres_sc(sc, resource, offset) ({			\
+#define NULL ((void*) 0)
+
+/*** Syscall macros **********************************************************/
+
+#define _sc1(sc, t1,a1) ({					\
       register uint64_t _res __asm("231");			\
-      register uint32_t _a1  __asm("231") = resource;		\
-      register uint32_t _a2  __asm("232") = offset;		\
+      register t1 _a1  __asm("231") = (a1);			\
+      __asm volatile ("trap 1,0,%1" 				\
+		      : "=r" (_res)				\
+		      : "i" (sc),				\
+		        "r" (_a1)				\
+		      : "memory");				\
+      _res;							\
+    })
+
+
+#define _sc2(sc, t1, a1, t2, a2) ({				\
+      register uint64_t _res __asm("231");			\
+      register t1 _a1  __asm("231") = (a1);			\
+      register t2 _a2  __asm("232") = (a2);			\
       __asm volatile ("trap 1,0,%1" 				\
 		      : "=r" (_res)				\
 		      : "i" (sc),				\
@@ -18,50 +38,108 @@ typedef unsigned long  uint64_t;
       _res;							\
     })
 
-#define _writeres_sc(sc, resource, offset, value) ({		\
-      register uint32_t _a1  __asm("231") = resource;		\
-      register uint32_t _a2  __asm("232") = offset;		\
-      register uint64_t _a3  __asm("233") = value;		\
-      __asm volatile ("trap 1,0,%0" 				\
-		      : 					\
+#define _sc3(sc, t1, a1, t2, a2, t3, a3) ({			\
+      register uint64_t _res __asm("231");			\
+      register t1 _a1  __asm("231") = (a1);			\
+      register t2 _a2  __asm("232") = (a2);			\
+      register t3 _a3  __asm("233") = (a3);			\
+      __asm volatile ("trap 1,0,%1" 				\
+		      : "=r" (_res)				\
 		      : "i" (sc),				\
 		        "r" (_a1), "r" (_a2), "r" (_a3)		\
 		      : "memory", "255");			\
+      _res;							\
     })
 
+
+/*** Byteswap functions ******************************************************/
+
+#define _mor(a1, a2)  ({					\
+      register uint64_t _res;					\
+      __asm("mor %0,%1,%2" : "=r" (_res) : "r" (a1), "r" (a2));	\
+      _res;							\
+    })
+
+
+static inline uint64_t bswap16(uint16_t x) {
+  return _mor(x, 0x0102);
+}
+
+static inline uint64_t bswap32(uint32_t x) {
+  return _mor(x, 0x01020304);
+}
+
+static inline uint64_t bswap64(uint64_t x) {
+  return _mor(x, 0x0102030405060708);
+}
+
+
+/*** Device functions ********************************************************/
+
+enum Endian {	/* SetEndian() flags */
+  ENDIAN_BIG	= 1,
+  ENDIAN_LITTLE	= 2,
+};
+
+enum {		/* MapResource() flags */
+  MAPF_READ	= 1,
+  MAPF_WRITE	= 2,
+};
+
+
 static inline uint64_t ReadResource8(uint32_t resource, uint32_t offset) {
-  return _readres_sc(0, resource, offset);
+  return _sc2(0, uint32_t, resource, uint32_t, offset);
 }
 
 static inline uint64_t ReadResource16(uint32_t resource, uint32_t offset) {
-  return _readres_sc(1, resource, offset);
+  return _sc2(1, uint32_t, resource, uint32_t, offset);
 }
 
 static inline uint64_t ReadResource32(uint32_t resource, uint32_t offset) {
-  return _readres_sc(2, resource, offset);
+  return _sc2(2, uint32_t, resource, uint32_t, offset);
 }
 
 static inline uint64_t ReadResource64(uint32_t resource, uint32_t offset) {
-  return _readres_sc(3, resource, offset);
+  return _sc2(3, uint32_t, resource, uint32_t, offset);
 }
 
 static inline void WriteResource8(uint32_t resource, uint32_t offset, uint8_t value) {
-  return _writeres_sc(4, resource, offset, value);
+  _sc3(4, uint32_t, resource, uint32_t, offset, uint8_t, value);
 }
 
 static inline void WriteResource16(uint32_t resource, uint32_t offset, uint16_t value) {
-  return _writeres_sc(5, resource, offset, value);
+  _sc3(5, uint32_t, resource, uint32_t, offset, uint16_t, value);
 }
 
 static inline void WriteResource32(uint32_t resource, uint32_t offset, uint32_t value) {
-  return _writeres_sc(6, resource, offset, value);
+  _sc3(6, uint32_t, resource, uint32_t, offset, uint32_t, value);
 }
 
 static inline void WriteResource64(uint32_t resource, uint32_t offset, uint64_t value) {
-  return _writeres_sc(7, resource, offset, value);
+  _sc3(7, uint32_t, resource, uint32_t, offset, uint64_t, value);
 }
 
-#undef _readres_sc
-#undef _writeres_sc
+static inline void SetEndian(enum Endian endian) {
+  _sc1(8, uint64_t, endian);
+}
+
+static inline uint64_t GetResourceSize(uint32_t resource) {
+  return _sc1(9, uint32_t, resource);
+}
+
+static inline void* MapResource(uint32_t resource, uint32_t flags, uint64_t* dma_addr) {
+  return (void*) _sc3(10, uint32_t, resource, uint32_t, flags, uint64_t*, dma_addr);
+}
+
+static inline void UnmapResource(void* addr) {
+  _sc1(11, void*, addr);
+}
+
+/*** Clean up ****************************************************************/
+
+#undef _mor
+#undef _sc1
+#undef _sc2
+#undef _sc3
 
 #endif /* pcode_calls_h */
