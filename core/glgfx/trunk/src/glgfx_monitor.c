4e94fc21ec3be03597f201b363ef93ef235529df
leviticus <leviticus@4f48108a-62f3-0310-b161-d5a2fa85e521>
2006-02-17 15:29:58 +0000

    r2220@elin:  lcs | 2006-02-17 16:17:20 +0100  Removed warning.

--- core/glgfx/trunk/src/glgfx_monitor.c
+++ core/glgfx/trunk/src/glgfx_monitor.c
@@ -64,7 +64,7 @@ static bool check_extensions(struct glgfx_monitor* monitor) {
 
 
   // Ugly hack for ATI's driver. FIXME!
-  monitor->miss_pixel_ops = strstr(glGetString(GL_VENDOR), "ATI ") != NULL;
+  monitor->miss_pixel_ops = strstr((char*) glGetString(GL_VENDOR), "ATI ") != NULL;
 
   // Check for framebuffer_object support
   if (g_hash_table_lookup(monitor->gl_extensions, "GL_EXT_framebuffer_object") != NULL) {
