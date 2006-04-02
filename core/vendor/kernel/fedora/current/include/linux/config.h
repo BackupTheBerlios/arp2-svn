0f3f661c4916e6e48f06d408e8860865c4400e34
leviticus <leviticus@4f48108a-62f3-0310-b161-d5a2fa85e521>
2006-04-02 12:24:59 +0000

    Removed.

--- core/vendor/kernel/fedora/current/include/linux/config.h
+++
@@ -1,8 +0,0 @@
-#ifndef _LINUX_CONFIG_H
-#define _LINUX_CONFIG_H
-
-#include <linux/autoconf.h>
-#if !defined (__KERNEL__) && !defined(__KERNGLUE__)
-#error including kernel header in userspace; use the glibc headers instead!
-#endif
-#endif
