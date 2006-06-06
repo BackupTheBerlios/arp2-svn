2b8c61b4f324f18750709987ec8296196a32c9f4
leviticus <leviticus@4f48108a-62f3-0310-b161-d5a2fa85e521>
2006-06-06 14:23:13 +0000

    Tag core/vendor/uae/current as core/vendor/uae/e-uae-0.8.29-WIP2.

---
+++ core/vendor/uae/e-uae-0.8.29-WIP2/src/targets/t-unix.h
@@ -0,0 +1,33 @@
+ /*
+  * UAE - The Un*x Amiga Emulator
+  *
+  * Target specific stuff, *nix version
+  *
+  * Copyright 1997 Bernd Schmidt
+  */
+
+#define TARGET_NAME "unix"
+
+#define TARGET_ROM_PATH         "~/"
+#define TARGET_FLOPPY_PATH      "~/"
+#define TARGET_HARDFILE_PATH    "~/"
+#define TARGET_SAVESTATE_PATH   "~/"
+
+#ifndef OPTIONSFILENAME
+# ifdef __APPLE__
+#  define OPTIONSFILENAME "default.uaerc"
+# else
+#  define OPTIONSFILENAME ".uaerc"
+# endif
+#endif
+#define OPTIONS_IN_HOME
+
+#define DEFPRTNAME "lpr"
+#define DEFSERNAME "/dev/ttyS1"
+
+#define write_log write_log_standard
+#define flush_log flush_log_standard
+
+#ifdef __APPLE__
+# define SLEEP_DONT_BUSY_WAIT
+#endif
