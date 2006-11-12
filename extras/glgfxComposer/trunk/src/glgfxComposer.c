5af308ef29b3731d3920207127a567b6677d5563
leviticus <leviticus@4f48108a-62f3-0310-b161-d5a2fa85e521>
2006-11-12 20:16:27 +0000

    Hey, it's not right, but at least the windows are kind of rendered on screen!!

--- extras/glgfxComposer/trunk/src/glgfxComposer.c
+++ extras/glgfxComposer/trunk/src/glgfxComposer.c
@@ -1,6 +1,7 @@
 
 #include <X11/Xlib.h>
 #include <X11/extensions/Xcomposite.h>
+#include <assert.h>
 #include <glgfx.h>
 #include <glgfx_bitmap.h>
 #include <glgfx_monitor.h>
@@ -28,37 +29,51 @@ struct screen {
 
 struct window {
     struct screen* screen;
+    XWindowAttributes attrs;
     Window window;
     Pixmap pixmap;
-    struct glgfx_viewport* viewport;
     struct glgfx_rasinfo* rasinfo;
     struct glgfx_bitmap* bitmap;
 };
 
 struct window* create_window(struct screen* screen, Window id) {
+  
+
+/*   printf("*** %d, %d %dx%dx%d b:%d %s\n",  */
+/* 	 attrs.x, attrs.y,  */
+/* 	 attrs.width, attrs.height, attrs.depth, */
+/* 	 attrs.border_width,  */
+/* 	 (attrs.map_state == IsViewable ? "IsViewable" :  */
+/* 	  attrs.map_state == IsUnviewable ? "IsUnviewable" : */
+/* 	  attrs.map_state == IsUnmapped ? "IsUnmapped" : "Unknown")); */
+
   struct window* window = calloc(sizeof *window, 1);
 
   if (window != NULL) {
-    XWindowAttributes attrs;
-
     window->screen = screen;
     window->window = id;
 
-    XGetWindowAttributes(screen->display, id, &attrs);
+    XGetWindowAttributes(screen->display, id, &window->attrs);
+
+    if (window->attrs.map_state != IsViewable || window->attrs.depth == 0) {
+      free(window);
+      return NULL;
+    }
 
     window->pixmap = XCompositeNameWindowPixmap(screen->display, id);
 /*     window->pixmap = XCreatePixmap(screen->display, window->window, 1000, 1000, 24); */
-    
+
+    printf("creating bitmap from pixmap %lx (window %lx)\n", (long) id, (long) window->pixmap);
     window->bitmap = glgfx_bitmap_create(
-      glgfx_bitmap_attr_width,    attrs.width,
-      glgfx_bitmap_attr_height,   attrs.height,
-      glgfx_bitmap_attr_bits,     attrs.depth,
-      glgfx_bitmap_attr_format,   glgfx_pixel_format_a8r8g8b8,
-      glgfx_bitmap_attr_visualid, XVisualIDFromVisual(attrs.visual),
+      glgfx_bitmap_attr_width,    window->attrs.width,
+      glgfx_bitmap_attr_height,   window->attrs.height,
+/*       glgfx_bitmap_attr_bits,     attrs.depth, */
+/*       glgfx_bitmap_attr_format,   glgfx_pixel_format_a8r8g8b8, */
+      glgfx_bitmap_attr_visualid, XVisualIDFromVisual(window->attrs.visual),
       glgfx_bitmap_attr_pixmap,   window->pixmap,
       glgfx_tag_end);
-    glgfx_bitmap_destroy(window->bitmap);
-    window->bitmap = NULL;
+/*     glgfx_bitmap_destroy(window->bitmap); */
+/*     window->bitmap = NULL; */
     
     printf("created window for id %x\n", (unsigned int) id);
   }
@@ -90,10 +105,27 @@ static void do_destroy_window(gpointer key, gpointer value, gpointer user_data) 
 }
 
 void add_window(struct screen* screen, struct window* window) {
-  g_hash_table_insert(screen->windows, (void*) window->window, window);
+  assert (window->rasinfo == NULL);
+
+  window->rasinfo = glgfx_viewport_addbitmap(screen->viewport, window->bitmap,
+					     glgfx_rasinfo_attr_x, window->attrs.x,
+					     glgfx_rasinfo_attr_y, window->attrs.y,
+					     glgfx_tag_end);
+
+  if (window->rasinfo != NULL) {
+    g_hash_table_insert(screen->windows, (void*) window->window, window);
+  }
+  else {
+    fprintf(stderr, "Unable to add bitmap to viewport\n");
+  }
 }
 
 void remove_window(struct screen* screen, struct window* window) {  
+  assert (window->rasinfo != NULL);
+
+  glgfx_viewport_rembitmap(screen->viewport, window->rasinfo);
+  window->rasinfo = NULL;
+
   g_hash_table_remove(screen->windows, (void*) window->window);
 }
 
@@ -111,6 +143,9 @@ void destroy_screen(struct screen* screen) {
     g_hash_table_destroy(screen->windows);
   }
 
+  glgfx_view_remviewport(screen->view, screen->viewport);
+  glgfx_viewport_destroy(screen->viewport);
+
   glgfx_monitor_setattrs(screen->monitor, 
 			 glgfx_monitor_attr_view, (intptr_t) NULL, 
 			 glgfx_tag_end);
@@ -126,6 +161,8 @@ void destroy_screen(struct screen* screen) {
 }
 
 struct screen* create_screen(char const* display, Display* d, int s) {
+  intptr_t width, height;
+
   struct screen* screen = calloc(sizeof *screen, 1);
 
   if (screen == NULL) { 
@@ -171,6 +208,15 @@ struct screen* create_screen(char const* display, Display* d, int s) {
     goto failed;
   }
 
+  if (glgfx_getattrs(screen->monitor,
+		     (glgfx_getattr_proto*) glgfx_monitor_getattr,
+		     glgfx_monitor_attr_width,  (intptr_t) &width,
+		     glgfx_monitor_attr_height, (intptr_t) &height,
+		     glgfx_tag_end) != 2) {
+    fprintf(stderr, "Unable to get display dimensions\n");
+    goto failed;
+  }
+
   screen->view = glgfx_view_create();
 
   if (screen->view == NULL) {
@@ -185,6 +231,19 @@ struct screen* create_screen(char const* display, Display* d, int s) {
     goto failed;
   }
   
+  screen->viewport = glgfx_viewport_create(glgfx_viewport_attr_width,  width,
+					   glgfx_viewport_attr_height, height,
+					   glgfx_tag_end);
+  
+  if (screen->viewport == NULL) {
+    fprintf(stderr, "Unable to create glgfx viewport.\n");
+    goto failed;
+  }
+
+  if (!glgfx_view_addviewport(screen->view, screen->viewport)) {
+    fprintf(stderr, "Unable to add glgfx viewport to view.\n");
+    goto failed;
+  }
   
   XGrabServer(d);
   
@@ -195,19 +254,16 @@ struct screen* create_screen(char const* display, Display* d, int s) {
   Window parent_return;
   Window* children;
   unsigned int num_children;
-  unsigned int i;
+  int i;
 	      
   XQueryTree(d, screen->root, &root_return, &parent_return, &children, &num_children);
-
+  
   for (i = 0; i < num_children; ++i) {
     struct window* window = create_window(screen, children[i]);
 
-    if (window == NULL) { 
-      XFree(children);
-      goto failed;
+    if (window != NULL) { 
+      add_window(screen, window);
     }
-
-    add_window(screen, window);
   }
 
   XFree(children);
@@ -336,7 +392,7 @@ int main(int argc, char const** argv) {
 	    rc = 20;
 	  }
 	  else {
-//	    rc = handle_screen(s);
+	    rc = handle_screen(s);
 	  }
 
 	  destroy_screen(s);
